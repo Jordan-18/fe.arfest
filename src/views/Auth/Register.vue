06b26ff106c0f61557ffc79b6fc4da3de0681c82
Jordan-18 <jordanistiqlal18@gmail.com>
2023-03-17 22:04:34 +0700

    login & register success

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -18,7 +18,7 @@
                 </div>
             </div>
             <div class="py-20">
-                <form class="form w-100" id="kt_sign_up_form" @submit.prevent="Register" data-kt-redirect-url="/login" action="#">
+                <form class="form w-100" id="kt_sign_up_form" @submit.prevent="Register" action="">
                     <div class="text-start mb-10">
                         <h1 class="text-dark mb-3 fs-3x" data-kt-translate="sign-up-title">Create an Account</h1>
                         <div class="text-gray-400 fw-semibold fs-6" data-kt-translate="general-desc">Get unlimited access & earn money</div>
@@ -61,8 +61,6 @@
                     <div class="d-flex flex-stack">
                         <button id="kt_sign_up_submit" class="btn btn-primary" data-kt-translate="sign-up-submit">
                             <span class="indicator-label">Submit</span>
-                            <span class="indicator-progress">Please wait...
-                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                         </button>
                         <div class="d-flex align-items-center">
                             <div class="text-gray-400 fw-semibold fs-6 me-6">Or</div>
@@ -80,75 +78,21 @@
                     </div>
                 </form>
             </div>
-            <div class="m-0">
-                <button class="btn btn-flex btn-link rotate" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px">
-                    <img data-kt-element="current-lang-flag" class="w-25px h-25px rounded-circle me-3" src="assets/media/flags/united-states.svg" alt="" />
-                    <span data-kt-element="current-lang-name" class="me-2">English</span>
-                    <span class="svg-icon svg-icon-3 svg-icon-muted rotate-180 m-0">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor" />
-                        </svg>
-                    </span>
-                </button>
-                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4" data-kt-menu="true" id="kt_auth_lang_menu">
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="English">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/united-states.svg" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">English</span>
-                        </a>
-                    </div>
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="Spanish">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/spain.svg" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">Spanish</span>
-                        </a>
-                    </div>
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="German">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/germany.svg" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">German</span>
-                        </a>
-                    </div>
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="Japanese">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/japan.svg" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">Japanese</span>
-                        </a>
-                    </div>
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="French">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/france.svg" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">French</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <div class="m-0"></div>
         </div>
     </div>
 </template>
-
 <script>
 import api from '@/api';
 export default {
     data(){
-        return {
-            users : []
-        }
+        return {}
     }, 
     mounted(){},
     methods:{
-        async  Register(){
+        async Register(){
             try {
+                Swal.showLoading()
                 const data = {
                     name    : this.firstName+' '+this.lastName, 
                     username: this.username, 
@@ -161,30 +105,39 @@ export default {
                         "Content-Type": "application/json",
                     },
                 })
-
                 if(response.status == 200){
                     let loggedIn    = true
-                    let token_type  = response.data.data.access_token_type
-                    let token       = response.data.data.token
+                    let token_type  = response.data.data.token_type
+                    let token       = response.data.data.access_token
                     let name        = response.data.data.user.name
                     let email       = response.data.data.user.email
                     let username    = response.data.data.user.username
 
 
-                    if(token){
-                        localStorage.setItem('loggedIn',loggedIn)
-                        localStorage.setItem('token_type',token_type)
-                        localStorage.setItem('token',token )
-                        localStorage.setItem('name',name)
-                        localStorage.setItem('email',email )
-                        localStorage.setItem('username',username)
-                    }
+                    localStorage.setItem('loggedIn',loggedIn)
+                    localStorage.setItem('token_type',token_type)
+                    localStorage.setItem('token',token )
+                    localStorage.setItem('name',name)
+                    localStorage.setItem('email',email )
+                    localStorage.setItem('username',username)
+
+                    this.$router.push({ 
+                        path: '/' 
+                    })
+                    Swal.fire('Register Successfully')
                 }else{
-                    
+                    Swal.fire('Something Wrong !')
+                    console.log(response);
                 }
-                console.log(response);
             } catch (error) {
-                console.error(error);
+                const err = await error
+                let message = `
+                    Error \n
+                    ${err.message} \n
+                    ${err.response.data.meta.message}
+                `
+                Swal.fire(message)
+                console.error(err);
             }
         }
     }
